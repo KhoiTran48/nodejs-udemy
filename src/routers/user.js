9017e89dc316141b2df1f946ddc1d989769ed3a7
Khoi Tran <khoitd@pascaliaasia.com>
2020-08-20 17:20:54 +0700

    resize avatar

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,7 @@ const router = new express.Router()
 const User = require("../models/user");
 const auth = require("../middleware/auth")
 const multer = require("multer")
+const sharp = require("sharp")
 
 router.post("/user", async (req, res)=>{
     const user = new User(req.body);
@@ -123,7 +124,8 @@ const upload = multer({
 })
 
 router.post("/users/me/avatar", auth, upload.single('inputFileName'), async (req, res)=>{
-    req.user.avatar = req.file.buffer
+    const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer()
+    req.user.avatar = buffer
     await req.user.save()
     res.send();
     
@@ -137,7 +139,7 @@ router.get("/user/:id/avatar", async (req, res)=>{
         if(!user || !user.avatar){
             throw new Error('no avatar')
         }
-        res.set('Content-Type', 'image/jpg')
+        res.set('Content-Type', 'image/png')
         res.send(user.avatar)
     } catch (error) {
         res.status(404).send()
