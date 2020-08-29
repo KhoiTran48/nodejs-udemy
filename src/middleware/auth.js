a99dbeee67ec65d7816fd6c7cbf1d31a2a0d194a
Khoi Tran <khoitd@pascaliaasia.com>
2020-08-29 22:30:37 +0700

    set env variables by env-cmd

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,7 +4,7 @@ const User = require("../models/user")
 const auth = async (req, res, next)=>{
     try {
         const token = req.header("Authorization")
-        const decoded = jwt.verify(token, "jwtkey")
+        const decoded = jwt.verify(token, process.env.JWT_KEY)
         const user = await User.findOne({ _id: decoded._id, "tokens.token": token})
         if(!user){
             throw new Error()
